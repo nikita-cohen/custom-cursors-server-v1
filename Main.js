557bb72cec9126b5988bc88637ae6373ac623cbe
nikita <nikitacohen7@gmail.com>
2022-01-18 20:18:58 +0200

    service ready

--- Main.js
+++ Main.js
@@ -6,6 +6,11 @@ const collectionRouter = require('./router/CollectionRouter');
 const userRouter = require("./router/UserRouter");
 const cursorRouter = require("./router/CursorRouter");
 const bodyParser = require('body-parser');
+const http = require('http');
+
+const hostname = '178.62.228.13';
+const port = 8089;
+
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
@@ -18,4 +23,12 @@ app.use('/collection', collectionRouter);
 app.use('/user', userRouter);
 app.use('/cursor', cursorRouter);
 
-app.listen(8089);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Sysmon App is Up and Running!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
